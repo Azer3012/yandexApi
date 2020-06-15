7cf39fd55d1a3198491c5dde38084ea9e176b8e4
Azer3012 <azer0297206@gmail.com>
2020-06-15 23:00:21 +0400

    yandexApiTranslate

---
+++ ui.js
@@ -0,0 +1,20 @@
+function UI(){
+
+    this.outputImage = document.getElementById("outputImage");
+    this.outputLanguage = document.getElementById("outputLanguage");
+    this.outputWord = document.getElementById("outputWord");
+
+    this.languageList = document.getElementById("language");
+
+
+}
+UI.prototype.changeUI = function(){
+    // Deyiştir
+
+    this.outputImage.src = `images/${this.languageList.value}.png`;
+
+}
+UI.prototype.displayTranslate = function(word) {
+    this.outputWord.textContent = word;
+    
+}
